42d8f1dec64df0dd358f48831adfc5435f1f480c
NicoSM89 <107883382+NicoSM89@users.noreply.github.com>
2022-10-23 21:06:02 -0300

    Entrega final
    Entrega Final

---
+++ jsonyeventos/contact.js
@@ -0,0 +1,40 @@
+var inputs = document.getElementsByClassName('formulario__input');
+for (var i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener('keyup', function () {
+        if (this.value.length >= 1) {
+            this.nextElementSibling.classList.add('fijar');
+        } else {
+            this.nextElementSibling.classList.remove('fijar');
+        }
+    });
+}
+
+const titulo = document.getElementsByClassName('formulario__input')
+const formulario = document.getElementById('formInicial')
+
+formulario.onsubmit = (event) => {
+    event.preventDefault()
+
+    for (const input of event.target.children) {
+        const obj = {}
+        obj['tipo'] = input.name
+        obj['valor'] = input.value
+        console.log(obj)
+        localStorage.setItem(obj.tipo,JSON.stringify,obj.valor)
+    }
+}
+
+const boton = document.getElementById('btn1')
+
+boton.onclick = mostrarAlert;
+
+function mostrarAlert() {
+        Swal.fire({
+        type: 'sucess',
+        title: 'Mensaje',
+        text: 'Formulario Enviado'
+    });
+};
+
+
+
